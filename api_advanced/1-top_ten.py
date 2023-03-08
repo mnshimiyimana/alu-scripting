6a10770cf8e64bba5c81a4383cde458d8cb67b8e
mnshimiyimana <m.nshimiyim@alustudent.com>
2023-03-08 23:14:59 +0000

    Work1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit
+"""Function that queries the Reddit API and prints the titles.
 """
 
 import json
